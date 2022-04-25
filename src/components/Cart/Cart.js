d39dd5f90c521bd961d18634ccc8d8c89aee277b
Shovon Khan <shovon@commlinkinfotech.com.bd>
2022-04-25 16:31:23 +0600

    shop page coding done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,12 +10,35 @@ const Cart = (props) => {
   //   const product = cart[i];
   //   total = total + product.price;
   // }
+  let shipping = 0;
+  if (totalPrice > 35) {
+    shipping = 0;
+  } else if (totalPrice > 15) {
+    shipping = 4.99;
+  } else if (totalPrice > 0) {
+    shipping = 12.99;
+  }
+
+  const tax = totalPrice / 10;
+  const grandTotal = totalPrice + shipping + Number(tax);
+
+  const formatNumber = (num) => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+  };
   return (
     <div>
       <h4>Order Summary</h4>
 
       <p>Item Ordered:{cart.length}</p>
-      <p>Total Price: {totalPrice}</p>
+      <p>Product Price: {formatNumber(totalPrice)}</p>
+      <p>
+        <small>Shipping Cost: {shipping}</small>
+      </p>
+      <p>
+        <small>Tax + VAT: {formatNumber(tax)}</small>
+      </p>
+      <p>Total Price: {formatNumber(grandTotal)}</p>
     </div>
   );
 };
